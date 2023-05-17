fafce01f22b4e23839f207ddc3cf9bbfc1c4c56d
Pratik Nugurwar <pratik.n@skyblue.aero>
2023-05-17 20:52:22 +0530

    Firebase eeded in environmet prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBvIxTkRuoQeEvL6MmCJnitH3X5rRsI0xg",
+    authDomain: "linkedinconnections-5b767.firebaseapp.com",
+    projectId: "linkedinconnections-5b767",
+    storageBucket: "linkedinconnections-5b767.appspot.com",
+    messagingSenderId: "909729770726",
+    appId: "1:909729770726:web:c55cbe69ca4de35a519f27"
+  }
 };
