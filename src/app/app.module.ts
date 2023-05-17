73764e1b50d031e028e872ff6b9a093e8a8173d5
Pratik Nugurwar <pratik.n@skyblue.aero>
2023-05-17 20:47:29 +0530

    import environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from 'src/environments/environment';
+import {environment} from '../environments/environment'
 import {provideFirebaseApp, initializeApp} from '@angular/fire/app'
 import {getFirestore, provideFirestore} from '@angular/fire/firestore'
 @NgModule({
