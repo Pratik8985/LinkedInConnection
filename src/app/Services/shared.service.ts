bb2a0212d27b9b7e9e6bcc5c0f0f5d694edf2b05
Pratik Nugurwar <pratik.n@skyblue.aero>
2023-05-27 16:58:15 +0530

    Platform Added

--- src/app/Services/shared.service.ts
+++ src/app/Services/shared.service.ts
@@ -6,7 +6,33 @@ import { Injectable } from '@angular/core';
 export class SharedService {
   public data:any
   public convertedData:any;
+  platform: string;
+
  
   // public data:{name:string, profession:string, technology:string, city:string , priority:string, notes:String}
-  constructor() { }
+  constructor() {
+    if (this.isMobile()) {
+      this.platform = 'Mobile browser';
+      console.log('here');
+    } else if (this.isDesktop()) {
+      this.platform = 'Desktop browser';
+      console.log('here1');
+
+    } else {
+      this.platform = 'Other platform';
+      console.log('here2');
+    }
+  }
+  public ngOnInit() {
+   
+  }
+  public isMobile(): boolean {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+  
+  
+  // Function to check if the platform is a desktop browser
+  public isDesktop(): boolean {
+    return !this.isMobile();
+  }
 }
